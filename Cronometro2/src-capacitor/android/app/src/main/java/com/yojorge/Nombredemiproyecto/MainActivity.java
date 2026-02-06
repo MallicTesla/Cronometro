ff12bf12e44b291c0bf997d72cc1fad3a980039c
MallicTesla <el_mallic@hotmail.com>
2026-02-06 11:28:57 -0300

    git ignore

--- Cronometro2/src-capacitor/android/app/src/main/java/com/yojorge/Nombredemiproyecto/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.yojorge.Nombredemiproyecto;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- Cronometro2/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <router-view />
-</template>
-
-<script setup>
-import './css/variables.css'
-import './css/app.css'
-import '@tabler/icons-webfont/dist/tabler-icons.min.css'
-</script>
